15d4fcd3cd478d8c4bca8108ec16ba4f391af22a
NicolasHov <kola.hov@gmail.com>
2017-10-25 13:18:29 +0200

    MAJ Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
+
   root to: 'static_pages#home'
-  
-  get '/home' , to: 'static_pages#home'
 
-  get '/contact', to: 'static_pages#contact'
+  get '/home' , to: 'static_pages#home', as: "home"
 
-  get '/about/:name', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact', as: "contact"
+
+  get '/about/:name', to: 'static_pages#about', as: "about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
