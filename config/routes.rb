1fd31ba0505a5e9dc700c9ab189f2874f52a167b
Nicolas Hovart <kola.hov@gmail.com>
2017-10-25 11:26:24 +0200

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  root to: 'static_pages#home'
+  
+  get '/home' , to: 'static_pages#home'
+
+  get '/contact', to: 'static_pages#contact'
+
+  get '/about/:name', to: 'static_pages#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
